424f31bde80f7eb5174cfb6e9477033e84bf8156
Alif Wibowo <alif.likes.coding@gmail.com>
2023-01-01 12:18:59 +0700

    Add function logic

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,29 @@
 def caesar_cipher(string, key)
-
+    string2 = ""
+    string.each_char do |char|
+        x = char.ord
+        if (x >= 65 && x <= 90)
+            y = x += key
+            if (y > 90)
+                y = y-90+65-1
+                string2.concat(y.chr)
+            else
+                string2.concat(y.chr)
+            end
+        elsif (x >= 97 && x <= 122)
+            y = x += key
+            if (y > 122)
+                y = y-122+97-1
+                string2.concat(y.chr)
+            else
+                string2.concat(y.chr)
+            end
+        else
+            string2.concat(x.chr)
+        end
+    end
+    string2
 end
+
+p caesar_cipher("What a string!", 5) #=> "Bmfy f xywnsl!"
+p caesar_cipher("ZzzAaa nice lol !@#$%^^", 3) #=> "CccDdd qlfh oro !@\#$%^^"
